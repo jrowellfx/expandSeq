8c03e48ea3bffe66442bcf36c668bbedc2891759
James Rowell <rowellogic-gh@yahoo.com>
2021-06-29 22:26:27 -0500

    wip

--- setup.py
+++ setup.py
@@ -7,12 +7,14 @@ here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
-    name     = 'expandSeq',
-    version  = '0.0.1',
-    packages = ['expandseq'],
-    url='https://github.com/jrowellfx/expandSeq',
-    author='James Philip Rowell',
-    author_email='james@alpha-eleven.com',
+    name            = 'expandSeq',
+    version         = '0.0.1',
+    packages        = ['expandseq'],
+    url             = 'https://github.com/jrowellfx/expandSeq',
+    author          = 'James Philip Rowell',
+    author_email    = 'james@alpha-eleven.com',
+    python_requires = '>=3.6, <4',
+
     entry_points = {
         'console_scripts': [
             'expandseq = expandseq.__main__:main',
