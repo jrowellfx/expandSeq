78c71a028cb7b1fcc023594da4200cd264762987
James Rowell <rowellogic-gh@yahoo.com>
2021-06-29 21:54:40 -0500

    wip

--- setup.py
+++ setup.py
@@ -7,37 +7,16 @@ here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
-    name='expandSeq',
-    version='0.0.1',
-    description='Command line utils to expose functionality of seqLister python library.',
-    long_description=long_description,
-    long_description_content_type='text/markdown',  # Optional (see note above)
+    name     = 'expandSeq',
+    version  = '0.0.1',
+    packages = ['expandseq'],
     url='https://github.com/jrowellfx/expandSeq',
     author='James Philip Rowell',
     author_email='james@alpha-eleven.com',
-
-    # Classifiers help users find your project by categorizing it.
-    #
-    # For a list of valid classifiers, see https://pypi.org/classifiers/
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: Linux',
-        'Operating System :: Unix',
-        'Operating System :: MacOS',
-        'Development Status :: 5 - Production/Stable',
-    ],
-
-    keywords='vfx, shot-td, image-sequence',
-    package_dir={'', 'src'},
-    packages=find_packages(where='src'),
-    python_requires='>=3.6, <4',
-    install_requires=['seqLister'],
-
-    entry_points={  # Optional
+    entry_points = {
         'console_scripts': [
-            'expandseq=expandseq.__main__:main',
-            'condenseseq=expandseq.__main__:main',
-        ],
-    },
+            'expandseq = expandseq.__main__:main',
+            'condenseseq = expandseq.__main__:main'
+        ]
+    }
 )
